ebc4fa4681aeb7136d2a58670ec8b70f150c9a17
andrevmarkelov <andrev_markelov@mail.ru>
2023-12-13 23:24:33 +0300

    adding block to home page

---
+++ view/block-layout.php
@@ -0,0 +1,23 @@
+<main>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-10 col-xl-8">
+                <div class="bg-shadow p-0 m-lg-0 p-lg-5">
+
+                    <h1 class="mb-4">Fulfillment dla Twojego <br> e-Commerce</h1>
+
+                    <ul class="p-0 mb-5">
+                        <li class="mb-2">Przyjęcie i magazynowanie produktów</li>
+                        <li class="mb-2">Kompletacja i pakowanie</li>
+                        <li class="mb-2">Obsługa zwrotów i reklamacji</li>
+                        <li class="mb-2">Współpraca ze wszystkimi dostępnymi przewoźnikami</li>
+                        <li class="mb-2">Integracja z Twoją platformą e-Commerce</li>
+                    </ul>
+
+                    <button class="button">Więcej</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
