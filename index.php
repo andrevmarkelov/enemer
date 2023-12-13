23ca050ef199596ea5421b82b09ac23a64782d89
andrevmarkelov <andrev_markelov@mail.ru>
2023-12-14 00:45:12 +0300

    adding cookie modal window

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Enemer</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
     <?php include_once 'view/block-layout.php'; ?>
+
+    <?php include_once 'view/cookie-popup.php'; ?>
 
     <script src="assets/js/bootstrap.js"></script>
     <script src="assets/js/script.js"></script>
